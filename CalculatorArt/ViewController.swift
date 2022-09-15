d6328adcc9081c3e9b6b46d013191d9193cff1ce
artemiosdev <flyboroda@gmail.com>
2022-09-15 23:41:25 +0300

    Add functional for buttons

--- CalculatorArt/ViewController.swift
+++ CalculatorArt/ViewController.swift
@@ -10,21 +10,45 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
-     
     
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
-    }
-
-    
-    @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
-    
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
     }
+    
+}
+
+
+@IBAction func numButtonPressed(_ sender: UIButton) {
+    
+    //What should happen when a number is entered into the keypad
+    
+}
 
 }
 
