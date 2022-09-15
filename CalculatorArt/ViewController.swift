32dc0aeb636e894915c96a9cd7633c225139fc01
artemiosdev <flyboroda@gmail.com>
2022-09-15 15:01:22 +0300

    Create the application layout

---
+++ CalculatorArt/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CalculatorArt
+//
+//  Created by Artem Androsenko on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+     
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}
+
