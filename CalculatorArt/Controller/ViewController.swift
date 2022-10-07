4dda936d172b9bc1b9d98fb04824f6147a079c8c
artemiosdev <flyboroda@gmail.com>
2022-10-07 23:56:09 +0300

    Add functionality to other operation buttons

--- CalculatorArt/Controller/ViewController.swift
+++ CalculatorArt/Controller/ViewController.swift
@@ -8,9 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var displayLabel: UILabel!
-    
     private var isFinishedTypingNumber: Bool = true
     private var displayValue: Double {
         get {
@@ -30,15 +28,16 @@ class ViewController: UIViewController {
         isFinishedTypingNumber = true
         calculator.setNumber(displayValue)
         if let calcMethod = sender.currentTitle {
-            guard let result = calculator.calculate(symbol: calcMethod) else {
-                fatalError("The result of the calculation is nil.")
+//            guard let result = calculator.calculate(symbol: calcMethod) else {
+//                fatalError("The result of the calculation is nil.")
+//            }
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            displayValue = result
         }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
         if let numValue = sender.currentTitle {
             
@@ -57,10 +56,7 @@ class ViewController: UIViewController {
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
-        
-        
     }
-    
 }
 
 
